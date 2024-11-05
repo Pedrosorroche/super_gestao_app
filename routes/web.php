bc9cd8c5fb37e3eb103737ff348aac11fdd8217b
Pedro <pedrosorroche.dev@gmail.com>
2024-11-05 13:06:13 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/contato', [ContatoController::class, 'contato']);
